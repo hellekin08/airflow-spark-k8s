8e93b7c19132e67fd203c0a95e87a09d86c53d42
uac7201 <maik.bialas@googlemail.com>
2025-08-24 11:51:36 +0200

    change dag

--- airflow/dags/spark.py
+++ airflow/dags/spark.py
@@ -2,21 +2,25 @@ from datetime import timedelta
 import os
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.utils.dates import days_ago
+from airflow.utils import timezone  # ✅ use this instead of days_ago
 from airflow.models import Variable
 
 default_args = {
     'owner': 'Howdy',
     'retries': 3,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
     'spark_on_k8s_airflow',
-    start_date=days_ago(1),
+    # Option A (dynamic): start yesterday from "now" in UTC
+    start_date=timezone.utcnow() - timedelta(days=1),
+    # Option B (static, recommended): uncomment the next line and set a fixed date
+    # start_date=timezone.datetime(2025, 8, 23, tz="UTC"),
     catchup=False,
-    schedule_interval=timedelta(days=1),
-    template_searchpath='/opt/airflow/dags/repo/dags/spark8s/'
+    schedule="@daily",                  # (equivalent shorthand)
+    template_searchpath='/opt/airflow/dags/repo/dags/spark8s/',
+    default_args=default_args,
 )
 
 spark_k8s_task = SparkKubernetesOperator(
@@ -28,7 +32,7 @@ spark_k8s_task = SparkKubernetesOperator(
     namespace="spark-operator",
     kubernetes_conn_id="kubernetes_default",
     do_xcom_push=True,
-    dag=dag
+    dag=dag,
 )
 
 spark_k8s_task
--- airflow/readme.md
+++ airflow/readme.md
@@ -11,9 +11,27 @@ helm upgrade --install airflow apache-airflow/airflow `
   --values values.yaml
 
 
+
+
 kubectl port-forward svc/airflow-api-server 8080:8080 --namespace airflow
 ```
 
+Github sync:
+```
+ssh-keygen -t rsa -b 4096 -C "maik.bialas@googlemail.com"
+
+cat C:\Users\maikb/.ssh/id_rsa.pub
+
+Github repo -> Settings -> Deploy keys -> Add deploy key ->
+Title: airflow-git-ssh
+Key: copy past content from the above file id_rsa.pub
 
 
+
+kubectl create secret generic airflow-git-ssh-secret `
+  --from-file=gitSshKey=C:\Users\maikb/.ssh/id_rsa `
+  --from-file=known_hosts=C:\Users\maikb/.ssh/known_hosts `
+  --from-file=id_ed25519.pub=C:\Users\maikb/.ssh/id_rsa.pub `
+  -n airflow
+```
 
