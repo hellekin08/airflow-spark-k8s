7f6e12225f20a0e556d850281e9531f74db79002
uac7201 <maik.bialas@googlemail.com>
2025-08-24 19:05:53 +0200

    full fix

--- airflow/dags/spark-example/spark.py
+++ airflow/dags/spark-example/spark.py
@@ -5,16 +5,14 @@ import os
 from airflow import DAG
 from airflow.utils import timezone
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
-# Paths: YAML is in the SAME folder as this file
 _THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = _THIS_DIR
 APP_FILE = "spark-app.yaml"
 
 default_args = {
     "owner": "Howdy",
-    "retries": 0,  
+    "retries": 0,
     "retry_delay": timedelta(minutes=5),
 }
 
@@ -29,20 +27,17 @@ with DAG(
         "spark_namespace": "spark-operator",
         "spark_image": "docker.io/library/spark:4.0.0",
         "executor_instances": 1,
-        "app_suffix": "",          
+        "app_suffix": "",
         "main_file": "local:///opt/spark/examples/src/main/python/pi.py",
-        "spark_app_name": "spark-on-k8s-airflow",
+        "spark_app_name": "spark-on-k8s-airflow",  # used by YAML metadata.name
     },
     tags=["spark", "kubernetes", "spark-operator"],
 ) as dag:
 
     submit_spark = SparkKubernetesOperator(
         task_id="submit_spark",
-        application_file=APP_FILE,
-        namespace="{{ params.spark_namespace }}",
+        application_file=APP_FILE,                 # Jinja-rendered using params
+        namespace="{{ params.spark_namespace }}",  # where the CR will be created
         kubernetes_conn_id="kubernetes_default",
-        application_name="{{ params.spark_app_name }}",   
-        do_xcom_push=False,                                
+        do_xcom_push=False,
     )
-
-    submit_spark 
